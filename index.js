bae798713bf2efecae26de03824f3169fa0e75fc
AbramsM1A2 <abraham.korez@gmail.com>
2021-03-12 16:44:36 +0100

    Servidor web

---
+++ index.js
@@ -0,0 +1,12 @@
+var cool = require("cool-ascii-faces");
+var express = require("express");
+var app = express();
+
+var port = 10000;
+app.get("/cool", (request,response) => {
+    response.send(cool());
+    console.log("new request to /cool has arrived");
+});
+app.listen(port, ()=> {
+    console.log("server ready listening on port "+port);
+});
