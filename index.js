33c4136be2d7463b7b0d9dfd9901b5b9cb097d1f
Lucia2712 <lucia271299@gmail.com>
2021-03-19 16:02:25 +0100

    creando heroku

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var cool = require("cool-ascii-faces");
 //console.log(cool());
 var express = require("express");
 var app = express();
+var path = require("path");
 
-var port = 10000;
+var port = (process.env.PORT || 10000);
+app.use("/", express.static(path.join(__dirname, "public")));
+
 app.get("/cool", (request,response) => {
     response.send(cool());
     console.log("new request to /cool has arrived");
