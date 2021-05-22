6b746725943155848fed5104c8716bd53b68bac2
19juaniyo99 <juanbuitrago1999@gmail.com>
2021-05-22 18:01:53 +0200

    primer uso de API BB

--- src/front/DivorceStatsAPI/integrations/BreakingBadAPI.svelte
+++ src/front/DivorceStatsAPI/integrations/BreakingBadAPI.svelte
@@ -1,177 +1,150 @@
 <script>
-    import {
-      Nav,
-      NavItem,
-      NavLink
-    } from "sveltestrap";
-    
-    
-    //API de los personajes de Breaking BAd por episodios 
-  var episodes = [];
-  var errorMsg = "";
-  var okMsg ="";
-  async function getStats() {
-      console.log("Fetching data...");
-  
-      const res = await fetch(
-        "https://breakingbadapi.com/api/episodes"
-      );
-  
-      if (res.ok) {
-        const json = await res.json();
-        episodes = json;
-  
-        console.log(`We have received ${episodes.length} episodes.`);
-      
-  
-        console.log("Ok");
-      } else {
-        errorMsg="Error al obtener los datos de los episodios"
-        okMsg = "";
-        console.log("ERROR!" + errorMsg);
-      }
-    };
-  
-    
-  
+    import { Nav, NavItem, NavLink } from "sveltestrap";
 
-  
-  am4core.ready(function() {
+    //API de los personajes de Breaking BAd por episodios
+    var episodes = [];
+    var errorMsg = "";
+    var okMsg = "";
+    async function getStats() {
+        console.log("Fetching data...");
 
-// Themes begin
-am4core.useTheme(am4themes_dark);
-am4core.useTheme(am4themes_animated);
-// Themes end
+        const res = await fetch("https://breakingbadapi.com/api/episodes");
 
+        if (res.ok) {
+            const json = await res.json();
+            episodes = json;
 
+            console.log(`We have received ${episodes.length} episodes.`);
 
-function processData(episodes) {
-    var treeData = [];
-
-    var irrelevantCharacters = { name: "Other", children: [] };
-
-    for (var episode in episodes) {
-        var episodeData = { name: episode_id, children: [] }
-        var episodeCharacters = 0;
-        for (var character in episodeData[characters]) {
-            episodeCharacters += episodeData[characters][character];
-        }
-
-
-        // only characters with more than 5 appearances
-        if (episodeCharacters >5) {
-            treeData.push(episodeData);
+            console.log("Ok");
+        } else {
+            errorMsg = "Error al obtener los datos de los episodios";
+            okMsg = "";
+            console.log("ERROR!" + errorMsg);
         }
     }
 
-    return treeData;
-}
+    am4core.ready(function () {
+        // Themes begin
+        am4core.useTheme(am4themes_dark);
+        am4core.useTheme(am4themes_animated);
+        // Themes end
 
-// create chart
-var chart = am4core.create("chartdiv", am4charts.TreeMap);
-chart.padding(0,0,0,0);
-chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
+        function processData(episodes) {
+            var treeData = [];
 
-// only one level visible initially
-chart.maxLevels = 2;
-// define data fields
-chart.dataFields.value = "count";
-chart.dataFields.name = "name";
-chart.dataFields.children = "children";
-chart.homeText = "Breaking Bad character´s appearance by episodes";
+            var irrelevantCharacters = { name: "Other", children: [] };
 
-// enable navigation
-chart.navigationBar = new am4charts.NavigationBar();
-chart.zoomable = false;
+            for (var episode in episodes) {
+                var episodeData = { name: episode_id, children: [] };
+                var episodeCharacters = 0;
+                for (var character in episodeData[characters]) {
+                    episodeCharacters += episodeData[characters][character];
+                }
 
-// level 0 series template
-var level0SeriesTemplate = chart.seriesTemplates.create("0");
-level0SeriesTemplate.strokeWidth = 2;
+                // only characters with more than 5 appearances
+                if (episodeCharacters > 5) {
+                    treeData.push(episodeData);
+                }
+            }
 
-// by default only current level series bullets are visible, but as we need brand bullets to be visible all the time, we modify it's hidden state
-level0SeriesTemplate.bulletsContainer.hiddenState.properties.opacity = 1;
-level0SeriesTemplate.bulletsContainer.hiddenState.properties.visible = true;
-// create hover state
-var columnTemplate = level0SeriesTemplate.columns.template;
-var hoverState = columnTemplate.states.create("hover");
+            return treeData;
+        }
 
-// darken
-hoverState.adapter.add("fill", function (fill, target) {
-    if (fill instanceof am4core.Color) {
-        return am4core.color(am4core.colors.brighten(fill.rgb, -0.2));
-    }
-    return fill;
-})
+        // create chart
+        var chart = am4core.create("chartdiv", am4charts.TreeMap);
+        chart.padding(0, 0, 0, 0);
+        chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
 
-// add logo
-var image = columnTemplate.createChild(am4core.Image);
-image.opacity = 0.15;
-image.align = "center";
-image.valign = "middle";
-image.width = am4core.percent(80);
-image.height = am4core.percent(80);
+        // only one level visible initially
+        chart.maxLevels = 2;
+        // define data fields
+        chart.dataFields.value = "count";
+        chart.dataFields.name = "name";
+        chart.dataFields.children = "children";
+        chart.homeText = "Breaking Bad character´s appearance by episodes";
 
-// add adapter for href to load correct image
-image.adapter.add("href", function (href, target) {
-    var dataItem = target.parent.dataItem;
-    if (dataItem) {
-        return "https://www.amcharts.com/lib/images/logos/" + dataItem.treeMapDataItem.name.toLowerCase() + ".png";
-    }
-});
+        // enable navigation
+        chart.navigationBar = new am4charts.NavigationBar();
+        chart.zoomable = false;
 
-// level1 series template
-var level1SeriesTemplate = chart.seriesTemplates.create("1");
-level1SeriesTemplate.columns.template.fillOpacity = 0;
-level1SeriesTemplate.columns.template.strokeOpacity = 0.4;
+        // level 0 series template
+        var level0SeriesTemplate = chart.seriesTemplates.create("0");
+        level0SeriesTemplate.strokeWidth = 2;
 
-var bullet1 = level1SeriesTemplate.bullets.push(new am4charts.LabelBullet());
-bullet1.locationX = 0.5;
-bullet1.locationY = 0.5;
-bullet1.label.text = "{name}";
-bullet1.label.fill = am4core.color("#ffffff");
-bullet1.label.fontSize = 9;
-bullet1.label.fillOpacity = 0.7;
+        // by default only current level series bullets are visible, but as we need brand bullets to be visible all the time, we modify it's hidden state
+        level0SeriesTemplate.bulletsContainer.hiddenState.properties.opacity = 1;
+        level0SeriesTemplate.bulletsContainer.hiddenState.properties.visible = true;
+        // create hover state
+        var columnTemplate = level0SeriesTemplate.columns.template;
+        var hoverState = columnTemplate.states.create("hover");
 
-chart.data = processData(data);
+        // darken
+        hoverState.adapter.add("fill", function (fill, target) {
+            if (fill instanceof am4core.Color) {
+                return am4core.color(am4core.colors.brighten(fill.rgb, -0.2));
+            }
+            return fill;
+        });
 
-}); // end am4core.ready()
+    
+
+        // level1 series template
+        var level1SeriesTemplate = chart.seriesTemplates.create("1");
+        level1SeriesTemplate.columns.template.fillOpacity = 0;
+        level1SeriesTemplate.columns.template.strokeOpacity = 0.4;
+
+        var bullet1 = level1SeriesTemplate.bullets.push(
+            new am4charts.LabelBullet()
+        );
+        bullet1.locationX = 0.5;
+        bullet1.locationY = 0.5;
+        bullet1.label.text = "{name}";
+        bullet1.label.fill = am4core.color("#ffffff");
+        bullet1.label.fontSize = 9;
+        bullet1.label.fillOpacity = 0.7;
+
+        chart.data = processData(episodes);
+    }); // end am4core.ready()
 </script>
-  
-  <svelte:head>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js" on:load={loadChart}></script>
-  </svelte:head>
-  
-  <main>
+
+<svelte:head>
+    <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/themes/dark.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+</svelte:head>
+
+<main>
     <Nav>
-      <NavItem>
-        <NavLink href="/">Página Principal</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/#/integrations/">volver</NavLink>
-      </NavItem>
+        <NavItem>
+            <NavLink href="/">Página Principal</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="/#/integrations/">volver</NavLink>
+        </NavItem>
     </Nav>
-  
+
     <div>
-      <h2>
-        Uso API externa de los personajes de la serie Breaking Bad 
-      </h2>
+        <h2>Uso API externa de los personajes de la serie Breaking Bad</h2>
     </div>
-  
+
     {#if errorMsg}
-      <p>{errorMsg}</p>
+        <p>{errorMsg}</p>
     {:else}
-    <div>
-      <canvas id="chart"></canvas>
-    </div>
+        <div>
+            <canvas id="chart" />
+        </div>
     {/if}
-  </main> 
+</main>
 
-
-
-  <style>
-    body { background-color: #30303d; color: #fff; }
-    #chartdiv {
-      width: 100%;
-      height: 700px;
+<style>
+    body {
+        background-color: #30303d;
+        color: #fff;
     }
-    </style>
+    #chartdiv {
+        width: 100%;
+        height: 700px;
+    }
+</style>
