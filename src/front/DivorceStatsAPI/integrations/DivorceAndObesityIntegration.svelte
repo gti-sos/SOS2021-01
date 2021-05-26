2314efb2cc98ccbd569dd9d68a6d5bac7604b91f
19juaniyo99 <juanbuitrago1999@gmail.com>
2021-05-26 13:31:32 +0200

    resolviendo problemas, Abraham te quiero

--- src/front/DivorceStatsAPI/integrations/DivorceAndObesityIntegration.svelte
+++ src/front/DivorceStatsAPI/integrations/DivorceAndObesityIntegration.svelte
@@ -56,18 +56,25 @@
         var paisesComunes = [];
 
         divorceStats.forEach((element) => {
-            if (divorceCountries[element.country] == null) {
-                divorceCountries.push(element);
+            if (!divorceCountries.includes(element.country)) {
+                divorceCountries.push(element.country);
             }
         }),
+        console.log("divorce countries",divorceCountries);
+
             obesityStats.forEach((element) => {
-                if (obesityCountries[element.country] == null) {
-                    obesityCountries.push(element);
+                if (!obesityCountries.includes(element.country)) {
+                    obesityCountries.push(element.country);
                 }
             }),
+        console.log("obesity countries", obesityCountries);
+
             (paisesComunes = divorceCountries.filter((value) =>
                 obesityCountries.includes(value)
             ));
+
+        console.log("paises comunes ", paisesComunes);
+
         divorceStats.forEach((e) => {
             var dato = {
                 country: "",
@@ -76,35 +83,44 @@
                 woman_obesity: null,
             };
 
-            if (
-                divorceCountries[e.country] != null &&
-                datos[{ country: e.country }] == null
-            ) {
+            if (paisesComunes.includes(e.country)){
+             
                 dato.country = e.country;
                 dato.divorceRate = e["divorce-rate"];
                 datos.push(dato);
             }
         }),
+
             obesityStats.forEach((e) => {
-                if (
-                    paisesComunes[e.country] != null &&
-                    datos[{ country: e.country }] != null
-                ) {
-                    dato = datos[{ country: e.country }];
-                    dato.man_obesity = e.man_percent;
-                    dato.woman_obesity = e.woman_percent;
-                    datos.update(dato);
+
+                if (paisesComunes.includes(e.country)) {
+
+                    for ( var i = 0; i< datos.length; i++){
+
+                        if(datos[i]["country"] == e.country){
+
+                            var dato1 = datos[i]
+                            dato1.man_obesity = e.man_percent;
+                            dato1.woman_obesity = e.woman_percent;
+                            datos[i]= dato1
+                            
+                        }
+
+                    }
                 }
+               
             });
         console.log("estos son los paises comunes ", paisesComunes);
+        console.log("estos son los datos ", datos);
         var serie1 = { name: "Divorce Rate", points: [] };
         var serie2 = { name: "Man Obesity", points: [] };
         var serie3 = { name: "Woman Obesity", points: [] };
 
         datos.forEach((e) => {
-            var point1 = { x: e.country, y: e["divorce-rate"] };
+            var point1 = { x: e.country, y: e.divorceRate };
             var point2 = { x: e.country, y: e.man_obesity };
             var point3 = { x: e.country, y: e.woman_obesity };
+            
             serie1.points.push(point1);
             serie2.points.push(point2);
             serie3.points.push(point3);
@@ -118,9 +134,9 @@
         var chart = JSC.chart("chartDiv", {
             debug: true,
             type: "column rounded",
-            title_label_text: "Acme Tool Sales",
-            yAxis: { label_text: "Measures" },
-            xAxis_label_text: "Country",
+            title_label_text: "Medición de los distintos porcentajes por paises, porcentaje de divorcios, porcentaje de obesidad masculina y obesidad femenina (entre los paises comunes de las APIs)",
+            yAxis: { label_text: "Medidas" },
+            xAxis_label_text: "Paises",
             series: [serie1, serie2, serie3],
         });
     }
@@ -143,7 +159,7 @@
     </Nav>
 
     <div>
-        <h2>Uso API externa Rick and Morty API</h2>
+        <h2>Integración entre Divorcios y Obesidad </h2>
     </div>
 
     {#if errorMsg}
