1872fa7022029b48341b954fe0271a29be0a190d
AbramsM1A2 <abraham.korez@gmail.com>
2021-05-26 12:06:55 +0200

    Update index.js

--- src/back/natality-statsAPI/v2/index.js
+++ src/back/natality-statsAPI/v2/index.js
@@ -20,7 +20,7 @@ module.exports.register = (app) => {
         db.remove({}, {multi: true});
         db.insert(initialdata);
         console.log(`Loaded initial data: <${JSON.stringify(initialdata, null, 2)}>`);
-        res.sendStatus(201);
+        res.sendStatus(200);
     });
 
     //GET /api/v1/natality-stats 
