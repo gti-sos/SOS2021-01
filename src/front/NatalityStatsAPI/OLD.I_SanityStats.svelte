0c09250ea635bc4e97311cc32e9ce39e5666c18f
AbramsM1A2 <abraham.korez@gmail.com>
2021-06-05 18:22:45 +0200

    natality integration change

--- src/front/NatalityStatsAPI/OLD.I_SanityStats.svelte
+++ src/front/NatalityStatsAPI/OLD.I_SanityStats.svelte
@@ -1,13 +1,14 @@
 <script>
   import { Nav, NavItem, NavLink, Spinner } from "sveltestrap";
 
+
   const BASE_CONTACT_API_PATH = "/api/v2";
 
   var sanityStats = [];
   var natalityData = [];
   var msg = "";
 
-  /**
+    /**
    * Carga los datos de la API SOS
    */
   async function loadApi() {
@@ -26,7 +27,7 @@
       }
     });
   }
-  /**
+ /**
    * Carga los datos de nuestra API
    */
   async function loadStats() {
@@ -45,7 +46,7 @@
       }
     });
   }
-  /**
+ /**
    * Obtiene los datos de nuestra API
    */
   async function getStats() {
@@ -64,7 +65,7 @@
       msg = "Error al cargar los datos de la API";
     }
   }
-  /**
+/**
    * Obtiene los datos de la API SOS
    */
   async function getSanityStats() {
@@ -84,7 +85,7 @@
       console.log("ERROR!" + msg);
     }
   }
-  /**
+ /**
    * Carga los datos en la grafica
    */
   async function loadChart() {
@@ -95,6 +96,7 @@
     var yAxis = [];
     var yAxis1 = [];
 
+    
     console.log("Calculating sanity-stats...");
     var index = 0;
     sanityStats.forEach((element) => {
@@ -105,7 +107,7 @@
         index++;
       }
     });
-
+    
     console.log("Calculating natality-stats...");
     natalityData.forEach((element) => {
       var e = element.country + "-" + element.date;
@@ -116,67 +118,55 @@
         }
       }
     });
-    //Como los años no coinciden, hay que situar correctamente
+    //Como los años no coinciden, hay que situar correctamente 
     // el eje x con el y
     var yAxis1 = [];
-    var yAxis2 = [];
     for (let i = 0; i < index; i++) {
       yAxis1.push(0);
     }
-    var yAxis2 = yAxis.slice(0, index);
-
-    var copy1 = yAxis.slice(index, yAxis.length);
-    for (let i = 0; i < copy1.length; i++) {
-      yAxis1.push(copy1[i]);
-    }
-    for (let i = 0; i < copy1.length; i++) {
-      yAxis2.push(0);
+    var copy = yAxis.slice(index, yAxis.lengt);
+    for (let i = 0; i < copy.length; i++) {
+      yAxis1.push(copy[i]);
     }
 
-    var options = {
-      series: [
-        {
-          name: "Gasto en sanidad (%)",
-          data: yAxis2,
-        },
-        {
-          name: "Ratio de natalidad (%)",
-          data: yAxis1,
-        },
-      ],
-      chart: {
-        type: "area",
-        width: 900,
-        height: 600,
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "straight",
-      },
+    var ctx = document.getElementById("myChart").getContext("2d");
 
-      title: {
-        text: "Comparativa del gasto en sanidad y del ratio de natalidad",
-        align: "center",
+    var myChart = new Chart(ctx, {
+      data: {
+        datasets: [
+          {
+            type: "line",
+            label: "Gasto en sanidad (%)",
+            data: yAxis.slice(0, index),
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+          },
+          {
+            type: "bar",
+            label: "Ratio de natalidad (%)",
+            data: yAxis1,
+            borderColor: "rgb(54, 162, 235)",
+          },
+        ],
+        labels: xAxis,
       },
-      labels: xAxis,
-      legend: {
-        position: "top",
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Comparativa del gasto en sanidad y del ratio de natalidad'
+            }
+        },
       },
-    };
-
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
+    });
   }
 </script>
 
 <svelte:head>
   <script
-    src="https://cdn.jsdelivr.net/npm/apexcharts"
+    src="https://cdn.jsdelivr.net/npm/chart.js"
     on:load={loadChart}></script>
 </svelte:head>
 
@@ -186,60 +176,40 @@
       <NavLink id="nav_home" href="/">Página Principal</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_integrations" href="/#/integrations/"
-        >Integraciones</NavLink
-      >
+      <NavLink id="nav_integrations" href="/#/integrations/">Integraciones</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_restcountries" href="/#/integrations/restcountries"
-        >restcountries</NavLink
-      >
+      <NavLink id="nav_restcountries" href="/#/integrations/restcountries">restcountries</NavLink>
     </NavItem>
     <NavItem>
       <NavLink id="nav_coinCap" href="/#/integrations/coinCap">coinCap</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_vatRates" href="/#/integrations/vatRates"
-        >vatRates</NavLink
-      >
+      <NavLink id="nav_vatRates" href="/#/integrations/vatRates">vatRates</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink disabled id="nav_sanityStats" href="/#/integrations/sanityStats"
-        >sanityStats</NavLink
-      >
+      <NavLink disabled id="nav_sanityStats" href="/#/integrations/sanityStats">sanityStats</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_platformsStats" href="/#/integrations/platformsStats"
-        >platformsStats</NavLink
-      >
+      <NavLink id="nav_platformsStats" href="/#/integrations/platformsStats">platformsStats</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_povertyRisks" href="/#/integrations/povertyRisks"
-        >povertyRisks</NavLink
-      >
+      <NavLink id="nav_povertyRisks" href="/#/integrations/povertyRisks">povertyRisks</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_illiteracy" href="/#/integrations/illiteracy"
-        >illiteracy</NavLink
-      >
+      <NavLink id="nav_illiteracy" href="/#/integrations/illiteracy">illiteracy</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_chidrenHIV" href="/#/integrations/chidrenHIV"
-        >chidrenHIV</NavLink
-      >
+      <NavLink id="nav_chidrenHIV" href="/#/integrations/chidrenHIV">chidrenHIV</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink
-        id="nav_childrenEmployment"
-        href="/#/integrations/childrenEmployment">childrenEmployment</NavLink
-      >
+      <NavLink id="nav_childrenEmployment" href="/#/integrations/childrenEmployment">childrenEmployment</NavLink>
     </NavItem>
     <NavItem>
-      <NavLink id="nav_unemployment" href="/#/integrations/unemployment"
-        >unemployment</NavLink
-      >
+      <NavLink id="nav_unemployment" href="/#/integrations/unemployment">unemployment</NavLink>
     </NavItem>
   </Nav>
+
 
   <div>
     <h2>Integración API SOS sanity-stats</h2>
@@ -250,8 +220,8 @@
     <p>{msg}</p>
     <Spinner color="primary" />
   {:else}
-    <div id="container">
-      <div id="chart" />
+    <div>
+      <canvas id="myChart" />
     </div>
   {/if}
 </main>
@@ -265,9 +235,8 @@
   div {
     margin-bottom: 15px;
   }
-  #container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  #myChart{
+    width: 400px;
+    height: 500px;
   }
 </style>
